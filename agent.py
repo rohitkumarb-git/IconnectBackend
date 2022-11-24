11ea037bc9cf5754663a8351515c4e6f3c51365d
rohitkumarb-git <116799614+rohitkumarb-git@users.noreply.github.com>
2022-11-24 19:29:29 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -67,7 +67,7 @@ class Agent:
         # print(meeting_list_for_day)
         return jsonify(meeting_list_for_day)
 
-    def agentAvailability(self,agent_id,date):
+    def agentAvailabileSlots(self,agent_id,date):
         agent_availability={"available_slots":["9:00","9:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30"]}
         scheduled_slots=[]
         meetings_list= self.agent_meetings_for_day(agent_id,date).get_json()
