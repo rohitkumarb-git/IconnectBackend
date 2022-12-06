902a2d9baa8446b2ebd5d9db726087ff0aebe6c2
rohitkumar <rohitkumar.b@outlook.com>
2022-12-06 20:07:07 +0530

    typo error corrected in line 16 &17

--- meetings.py
+++ meetings.py
@@ -13,8 +13,8 @@ class Meetings:
             "_id":uuid.uuid4().hex,
             "agent_id":request.get_json()["agent_id"],
             "user_id":request.get_json()["user_id"],
-            "user_name": db.users.find({"_id":request.get_json()['user_id']})["name"],
-            "agent_name":db.agents_profile.find({"_id":request.get_json()['agent_id']})["name"],
+            "user_name": db.users.find_one({"_id":request.get_json()['user_id']})["name"],
+            "agent_name":db.agents_profile.find_one({"_id":request.get_json()['agent_id']})["name"],
             "meeting_details":request.get_json()["meeting_details"],
         }
         
