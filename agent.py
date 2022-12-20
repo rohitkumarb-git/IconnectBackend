f5bbdff5f539bad8fbecd9e89fb13f6609d9c371
rohitkumar <rohitkumar.b@outlook.com>
2022-12-20 16:13:03 +0530

    .

--- agent.py
+++ agent.py
@@ -65,7 +65,7 @@ class Agent:
             if meeting["meeting_details"]["scheduled_date"]==date:
                 meeting_list_for_day.append(meeting)
         # print(meeting_list_for_day)
-        return jsonify(meeting_list_for_day)
+        return (meeting_list_for_day)
 
     def agentAvailabileSlots(self,agent_id,date):
         agent_availability={"available_slots":["9:00","9:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30"]}
